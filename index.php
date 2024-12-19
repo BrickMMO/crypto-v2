1812e941aed30bceaf01043e3f342c091f9821f2
Adam <thomasadam83@hotmail.com>
2024-12-18 19:44:10 -0500

    Added evn variables:

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loot Node</title>
+</head>
+<body>
+
+    <h1>Loot Node</h1>
+
+    <?php 
+
+    echo DOMAIN;
+
+    ?>
+    
+</body>
+</html>
